4ce97bb1d2f66e062be5a17abe1ec1398b690c6c
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-05-30 18:21:08 +0900

    Banner

--- springboothttp/README.md
+++ springboothttp/README.md
@@ -6,3 +6,6 @@ keytool -genkey
         -keystore keystore.p12 
         -validity 4000
         
+        
+        
+vmOption -debug 모드 실행시 디버그 모드
--- springboothttp/src/main/java/com/kingname/springboothttp/SpringboothttpApplication.java
+++ springboothttp/src/main/java/com/kingname/springboothttp/SpringboothttpApplication.java
@@ -1,5 +1,6 @@
 package com.kingname.springboothttp;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,19 @@ public class SpringboothttpApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringboothttpApplication.class, args);
+
+        //SpringApplication.run(SpringboothttpApplication.class, args);
+
+        SpringApplication app = new SpringApplication(SpringboothttpApplication.class);
+        app.setBanner(((environment, sourceClass, out) -> {
+            out.println("===================");
+            out.println("LimGeun");
+            out.println("===================");
+        }));
+
+        //app.setBannerMode(Banner.Mode.OFF);
+
+        app.run(args);
     }
 
 //    @Bean
