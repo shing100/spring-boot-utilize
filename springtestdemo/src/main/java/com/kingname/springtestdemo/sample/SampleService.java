e61190e8577e9dc6b1400c231bdd5f222a80b037
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-06-12 22:36:24 +0900

    Spring boot Test

---
+++ springtestdemo/src/main/java/com/kingname/springtestdemo/sample/SampleService.java
@@ -0,0 +1,11 @@
+package com.kingname.springtestdemo.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "kingname";
+    }
+}
