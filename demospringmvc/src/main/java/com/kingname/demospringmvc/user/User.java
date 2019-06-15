772d621fba297a66be5ac841a286f5628b2378fa
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-06-15 23:59:21 +0900

    HttpMessageConvertes

---
+++ demospringmvc/src/main/java/com/kingname/demospringmvc/user/User.java
@@ -0,0 +1,14 @@
+package com.kingname.demospringmvc.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+}
