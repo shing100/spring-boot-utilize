e76d6cd69252547184ed2de96e16e69d98456b68
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-06-18 21:18:04 +0900

    MVC 정적 리소스 지원

--- README.md
+++ README.md
@@ -389,3 +389,26 @@ howto-logging.html 커스텀 로그 설정 파일 사용하기
    <version>2.9.6</version>
 </dependency>
 ```
+
+## 정적 리소스 맵핑 “ /**”
+
+### 기본 리소스 위치
+- classpath:/static
+- classpath:/public
+- classpath:/resources/
+- classpath:/META-INF/resources
+   - 예) “/hello.html” => /static/hello.html
+
+- spring.mvc.static-path-pattern: 맵핑 설정 변경 가능
+- spring.mvc.static-locations: 리소스 찾을 위치 변경 가능
+- Last-Modified 헤더를 보고 304 응답을 보냄.
+    - ResourceHttpRequestHandler가 처리함.
+
+- WebMvcConfigurer의 addRersourceHandlers로 커스터마이징 할 수 있음
+```java
+@Override
+public void addResourceHandlers(ResourceHandlerRegistry registry) {
+  registry.addResourceHandler("/m/**")
+    .addResourceLocations("classpath:/m/")
+    .setCachePeriod(20);
+```
--- demospringmvc/src/main/java/com/kingname/demospringmvc/config/WebConfig.java
+++ demospringmvc/src/main/java/com/kingname/demospringmvc/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.kingname.demospringmvc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,5 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/hello");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")
+                .addResourceLocations("classpath:/m/")
+                .setCachePeriod(20);
     }
 }
