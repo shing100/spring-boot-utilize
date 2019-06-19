7ad4226b53be5914a72939ed3f18fc8e95246bd9
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-06-19 23:01:11 +0900

    Thymeleaf

---
+++ springmvcdemo/src/test/java/com/kingname/springmvcdemo/SampleControllerTest.java
@@ -0,0 +1,37 @@
+package com.kingname.springmvcdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception{
+        // 요청 "/hello"
+        // 응답
+        // - 모델 name : kingname
+        // - 뷰 이름 : hello
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"))
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("kingname")))
+                .andExpect(content().string(containsString("kingname")));
+    }
+
+}
