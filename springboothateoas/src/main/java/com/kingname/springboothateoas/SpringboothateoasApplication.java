629c5c256868343a16603b9cb5b5434cc747f90f
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-06-22 11:03:46 +0900

    HATEOAS

---
+++ springboothateoas/src/main/java/com/kingname/springboothateoas/SpringboothateoasApplication.java
@@ -0,0 +1,13 @@
+package com.kingname.springboothateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboothateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboothateoasApplication.class, args);
+    }
+
+}
