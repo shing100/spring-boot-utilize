10f0cc543db03065b946f25fc4ed1e9464df8cfa
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-06-22 10:45:43 +0900

    ErrorHandler

--- springmvcdemo/src/main/java/com/kingname/springmvcdemo/SampleController.java
+++ springmvcdemo/src/main/java/com/kingname/springmvcdemo/SampleController.java
@@ -2,7 +2,9 @@ package com.kingname.springmvcdemo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
@@ -12,4 +14,18 @@ public class SampleController {
         model.addAttribute("name", "kingname");
         return "hello";
     }
+
+    @GetMapping("/exception")
+    public String exception() {
+        throw new SampleException();
+    }
+
+    @ExceptionHandler(SampleException.class)
+    public @ResponseBody AppError sampleError(SampleException e) {
+        AppError appError = new AppError();
+        appError.setMessage("error.app.key");
+        appError.setError("IDK IDK IDK");
+        return appError;
+    }
+
 }
---
+++ springmvcdemo/src/main/java/com/kingname/springmvcdemo/SampleException.java
@@ -0,0 +1,4 @@
+package com.kingname.springmvcdemo;
+
+public class SampleException extends RuntimeException {
+}
